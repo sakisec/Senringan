15d472d4274f7a4941cd47244e4856b20a05abf1
Saki Iwakura <236205357+sakisec@users.noreply.github.com>
2025-10-16 19:17:35 +0200

    Add files via upload

---
+++ Modules/langscan/target/x86_64-pc-windows-gnu/release/senringan-langscan.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/Valencia/Documents/Senringan/Modules/langscan/target/x86_64-pc-windows-gnu/release/senringan-langscan.exe: /mnt/c/Users/Valencia/Documents/Senringan/Modules/langscan/src/main.rs
